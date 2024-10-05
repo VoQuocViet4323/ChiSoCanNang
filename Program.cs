6ba690ed77c44f6dbeea289e061e5d5c74e03053
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-05 22:16:12 +0700

    a

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double height;
+        double weight;
+        Console.WriteLine("Nhap chieu cao");
+        height = float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap can nang");
+        weight = float.Parse(Console.ReadLine());
+        double bmi = weight / Math.Pow(height, 2);
+        bmi = Math.Round(bmi,1);
+        Console.WriteLine("BMI: " + bmi);
+        if(bmi < 18){
+            Console.WriteLine("Thieu' can");            
+        }else if(bmi < 25){
+            Console.WriteLine("Binh` thuong`");
+        }else if(bmi < 30){
+            Console.WriteLine("Thua` can");
+        }else 
+            Console.WriteLine($"Beo' phi`");
+    }
+}
